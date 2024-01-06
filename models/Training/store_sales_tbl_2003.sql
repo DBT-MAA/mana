270d0856c5bf3858350f049e92a20b4e7466e1b0
d d <dbdbt2024@gmail.com>
2024-01-06 11:58:16 +0000

    commit changes

--- models/Training/store_sales_tbl_2003.sql
+++ models/Training/store_sales_tbl_2003.sql
@@ -1,3 +1,5 @@
-with store_sales_tbl_2003 as
+{{ config(materialized='table') }}
+
+ with store_sales_tbl_2003 as
 (select ss_store_sk,ss_quantity,ss_sales_price,ss_net_profit from  DBT_DB1.DEV.store_sales_2003)
-select * from store_sales_tbl_2003
+select * from store_sales_tbl_2003 
--- models/Training/store_tbl_2003.sql
+++ models/Training/store_tbl_2003.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='table') }}
 
 with store_tbl_2003 as
 (select s_store_sk,s_store_id,s_store_name,s_country from DBT_DB1.DEV.store_2003)
