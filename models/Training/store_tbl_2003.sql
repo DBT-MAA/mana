67c0869f3d0e9a596eb0e3975bd7b221d2168d3f
d d <dbdbt2024@gmail.com>
2024-01-06 10:25:05 +0000

    commit changes

--- models/Training/store_tbl_2003.sql
+++ models/Training/store_tbl_2003.sql
@@ -1,4 +1,4 @@
 
 with store_tbl_2003 as
-(select top 10 * from DBT_DB1.DEV.store_2003)
+(select s_store_sk,s_store_id,s_store_name,s_country from DBT_DB1.DEV.store_2003)
 select * from store_tbl_2003 
