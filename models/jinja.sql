262ec1cb7b15d167aef2118bbf8cf161d94d1487
d d <dbdbt2024@gmail.com>
2024-01-10 07:23:36 +0000

    commit changes

--- models/jinja.sql
+++ models/jinja.sql
@@ -1,2 +1,12 @@
-{% set value=123 %}
-{{value}}
+{%- set COUNTRY = 'USA','UK','GERMANY','canada','japan' -%}
+{%- for COUNTRY in COUNTRY -%}
+my country is : {{ COUNTRY | capitalize }}
+{% endfor %}
+
+{% set my_score=67 %}
+{%- set passing_score=60 -%}
+{%- if my_score > passing_score -%}
+you have pass the exame
+{%- else -%}
+you have fail
+{%- endif -%}
